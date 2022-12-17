9f657b28322586e4fcf148a15b15da99927510fe
tmeir <66691248+tmeir@users.noreply.github.com>
2022-12-17 14:05:38 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
-/* This file is based on the file from http://ha.ckers.org/xss.js
-   It has been reproduced here due to the extended downtime of ha.ckers.org
-   This file is being hosted as a courtesy to the security community.
-*/
 
-document.write ("This is remote text via xss.js located at xss.rocks " + document.cookie);
-alert ("This is remote text via xss.js located at xss.rocks " + document.cookie);
 
-AKIAIOSFODNN7EXAMPLE
-dfdfd
-var x = x
-test
-echo
-www.firebaseio.com
+document.write("This is remote text via zz.js located at zz.rocks " + document.cookie);
+alert("This is remote text via zz.js located at zz.rocks " + document.cookie);
+print(12345)
+AKIAIOSFODNN7EXAMPLE;
+dfdfd;
+var x = x;
+test;
+echo;
+www.firebaseio.com;
 
